efc6627f6165d1a59d324013dda8a42e94794844
neil.tao <neil.tao@titansoft>
2018-11-30 23:05:52 +0800

    finish

--- BudgetServiceTdd/BudgetService.cs
+++ BudgetServiceTdd/BudgetService.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BudgetServiceTdd
 {
-	public class BudgetService
-	{
-		public double TotalAmount(DateTime start, DateTime end)
-		{
-			throw new NotImplementedException();
-		}
-	}
+    public class BudgetService
+    {
+        private readonly Dictionary<string, int> _budgetLookUp;
+        private readonly IBudgetRepository _budgetRepository;
+
+        public BudgetService(IBudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+            _budgetLookUp = SetBudgetToLookUp();
+        }
+
+        public Dictionary<string, int> SetBudgetToLookUp()
+        {
+            var budgets = _budgetRepository.GetAll();
+            return budgets.ToDictionary(x => x.YearMonth, x => x.Amount / GetDays(x.YearMonth));
+        }
+
+        public double TotalAmount(DateTime start, DateTime end)
+        {
+            return start > end ? 0 : GetYearMonthList(start, end).Sum();
+        }
+
+        private int GetDays(string yearMonth)
+        {
+            var year = Convert.ToInt32(yearMonth.Substring(0, 4));
+            var month = Convert.ToInt32(yearMonth.Substring(4, 2));
+            return DateTime.DaysInMonth(year, month);
+        }
+
+        private IEnumerable<int> GetYearMonthList(DateTime startDate, DateTime endDate)
+        {
+            for (var year = startDate.Year; year <= endDate.Year; year++)
+            {
+                for (var month = startDate.Month; month <= endDate.Month; month++)
+                {
+                    var yearMonth = year + TransToDateFormat(month);
+                    var budget = _budgetLookUp.ContainsKey(yearMonth) ? _budgetLookUp[yearMonth] : 0;
+                    if (IsLsatMonth(endDate, month, year))
+                    {
+                        yield return endDate.Day * budget;
+                    }
+                    else if (IsFirstMonth(startDate, month, year))
+                    {
+                        yield return (DateTime.DaysInMonth(year, month) - startDate.Day + 1) * budget;
+                    }
+                    else
+                    {
+                        yield return DateTime.DaysInMonth(year, month) * budget;
+                    }
+                }
+            }
+        }
+
+        private bool IsFirstMonth(DateTime start, int month, int year)
+        {
+            return month == start.Month && year == start.Year;
+        }
+
+        private bool IsLsatMonth(DateTime endDate, int month, int year)
+        {
+            return month == endDate.Month && year == endDate.Year;
+        }
+
+        private string TransToDateFormat(int i)
+        {
+            return i < 10 ? "0" + i : i.ToString();
+        }
+    }
 }
--- BudgetServiceTddTests/BudgetServiceTests.cs
+++ BudgetServiceTddTests/BudgetServiceTests.cs
@@ -1,14 +1,90 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace BudgetServiceTdd.Tests
 {
-	[TestClass()]
-	public class BudgetServiceTests
-	{
-		[TestMethod]
-		public void MyTestMethod()
-		{
-			Assert.Fail("no implement");
-		}
-	}
+    [TestClass()]
+    public class BudgetServiceTests
+    {
+        private readonly FakeRepository _budgetRepository = new FakeRepository();
+        private readonly BudgetService _budgetService;
+        private DateTime _endDate;
+        private DateTime _startDate;
+
+        public BudgetServiceTests()
+        {
+            _budgetService = new BudgetService(_budgetRepository);
+        }
+
+        [TestMethod]
+        public void Cross_two_months()
+        {
+            _startDate = new DateTime(2018, 4, 30);
+            _endDate = new DateTime(2018, 5, 2);
+            TheBudgetShouldBe(12);
+        }
+
+        [TestMethod]
+        public void Only_one_month_sum_of_2days()
+        {
+            _startDate = new DateTime(2018, 4, 1);
+            _endDate = new DateTime(2018, 4, 2);
+            TheBudgetShouldBe(20);
+        }
+
+        [TestMethod]
+        public void StartDate_bigger_than_EndDate()
+        {
+            _startDate = new DateTime(2018, 4, 30);
+            _endDate = new DateTime(2018, 4, 1);
+            TheBudgetShouldBe(0);
+        }
+
+        [TestMethod]
+        public void The_date_never_be_set()
+        {
+            _startDate = new DateTime(2018, 9, 1);
+            _endDate = new DateTime(2018, 9, 2);
+            TheBudgetShouldBe(0);
+        }
+
+        [TestMethod]
+        public void Trans_to_dictionary_test()
+        {
+            var budgetToLookUp = _budgetService.SetBudgetToLookUp();
+            var expected = new Dictionary<string, int>()
+            {
+                { "201804",10},
+                { "201805",1}
+            };
+            expected.ToExpectedObject().ShouldEqual(budgetToLookUp);
+        }
+
+        private void TheBudgetShouldBe(int expected)
+        {
+            Assert.AreEqual(expected, _budgetService.TotalAmount(_startDate, _endDate));
+        }
+    }
+
+    internal class FakeRepository : IBudgetRepository
+    {
+        public List<Budget> GetAll()
+        {
+            return new List<Budget>()
+            {
+                new Budget
+                {
+                    YearMonth = "201804",
+                    Amount = 300
+                },
+                new Budget
+                {
+                    YearMonth = "201805",
+                    Amount = 31
+                }
+            };
+        }
+    }
 }
