4004e37a8a25233a05ba1508a409cf62203b5d21
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-12-01 21:16:27 +0800

    remove Trans_to_dictionary_test test, due to not use dictionary any more

--- BudgetServiceTdd/BudgetService.cs
+++ BudgetServiceTdd/BudgetService.cs
@@ -13,30 +13,18 @@ namespace BudgetServiceTdd
 			_budgetRepository = budgetRepository;
 		}
 
-		public Dictionary<string, int> SetBudgetToLookUp()
-		{
-			var budgets = _budgetRepository.GetAll();
-			return budgets.ToDictionary(x => x.YearMonth, x => x.Amount / GetDays(x.YearMonth));
-		}
 
 		public double TotalAmount(DateTime start, DateTime end)
 		{
 			return start > end ? 0 : GetYearMonthList(start, end).Sum();
 		}
 
-		private int GetDays(string yearMonth)
-		{
-			var year = Convert.ToInt32(yearMonth.Substring(0, 4));
-			var month = Convert.ToInt32(yearMonth.Substring(4, 2));
-			return DateTime.DaysInMonth(year, month);
-		}
-
 		// primitive obsession, data clump, duplication
 		private IEnumerable<int> GetYearMonthList(DateTime startDate, DateTime endDate)
 		{
 			var period = new Period(startDate, endDate);
-			var budgets = _budgetRepository.GetAll();
-			return budgets.Select(b => b.DailyAmount() * period.OverlappingDays(b.YearMonthInDateTime));
+			return _budgetRepository.GetAll()
+				.Select(b => b.DailyAmount() * period.OverlappingDays(b.YearMonthInDateTime));
 		}
 	}
 }
--- BudgetServiceTddTests/BudgetServiceTests.cs
+++ BudgetServiceTddTests/BudgetServiceTests.cs
@@ -1,117 +1,103 @@
-﻿using ExpectedObjects;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
 namespace BudgetServiceTdd.Tests
 {
-    [TestClass()]
-    public class BudgetServiceTests
-    {
-        private readonly FakeRepository _budgetRepository = new FakeRepository();
-        private readonly BudgetService _budgetService;
-        private DateTime _endDate;
-        private DateTime _startDate;
+	[TestClass()]
+	public class BudgetServiceTests
+	{
+		private readonly FakeRepository _budgetRepository = new FakeRepository();
+		private readonly BudgetService _budgetService;
+		private DateTime _endDate;
+		private DateTime _startDate;
 
-        public BudgetServiceTests()
-        {
-            _budgetService = new BudgetService(_budgetRepository);
-        }
+		public BudgetServiceTests()
+		{
+			_budgetService = new BudgetService(_budgetRepository);
+		}
 
-        [TestMethod]
-        public void Cross_2years()
-        {
-            _startDate = new DateTime(2017, 12, 31);
-            _endDate = new DateTime(2019, 1, 3);
-            TheBudgetShouldBe(365);
-        }
+		[TestMethod]
+		public void Cross_2years()
+		{
+			_startDate = new DateTime(2017, 12, 31);
+			_endDate = new DateTime(2019, 1, 3);
+			TheBudgetShouldBe(365);
+		}
 
-        [TestMethod]
-        public void Cross_two_months()
-        {
-            _startDate = new DateTime(2018, 4, 30);
-            _endDate = new DateTime(2018, 5, 2);
-            TheBudgetShouldBe(12);
-        }
+		[TestMethod]
+		public void Cross_two_months()
+		{
+			_startDate = new DateTime(2018, 4, 30);
+			_endDate = new DateTime(2018, 5, 2);
+			TheBudgetShouldBe(12);
+		}
 
-        [TestMethod]
-        public void Cross_years()
-        {
-            _startDate = new DateTime(2017, 12, 31);
-            _endDate = new DateTime(2018, 1, 3);
-            TheBudgetShouldBe(3);
-        }
-        [TestMethod]
-        public void Only_one_month_sum_of_2days()
-        {
-            _startDate = new DateTime(2018, 4, 1);
-            _endDate = new DateTime(2018, 4, 2);
-            TheBudgetShouldBe(20);
-        }
+		[TestMethod]
+		public void Cross_years()
+		{
+			_startDate = new DateTime(2017, 12, 31);
+			_endDate = new DateTime(2018, 1, 3);
+			TheBudgetShouldBe(3);
+		}
 
-        [TestMethod]
-        public void StartDate_bigger_than_EndDate()
-        {
-            _startDate = new DateTime(2018, 4, 30);
-            _endDate = new DateTime(2018, 4, 1);
-            TheBudgetShouldBe(0);
-        }
+		[TestMethod]
+		public void Only_one_month_sum_of_2days()
+		{
+			_startDate = new DateTime(2018, 4, 1);
+			_endDate = new DateTime(2018, 4, 2);
+			TheBudgetShouldBe(20);
+		}
 
-        [TestMethod]
-        public void The_date_never_be_set()
-        {
-            _startDate = new DateTime(2018, 9, 1);
-            _endDate = new DateTime(2018, 9, 2);
-            TheBudgetShouldBe(0);
-        }
+		[TestMethod]
+		public void StartDate_bigger_than_EndDate()
+		{
+			_startDate = new DateTime(2018, 4, 30);
+			_endDate = new DateTime(2018, 4, 1);
+			TheBudgetShouldBe(0);
+		}
 
-        [TestMethod]
-        public void Trans_to_dictionary_test()
-        {
-            var budgetToLookUp = _budgetService.SetBudgetToLookUp();
-            var expected = new Dictionary<string, int>()
-            {
-                {"201801",1},
-                {"201804",10},
-                {"201805",1},
-                {"201901",1},
-            };
-            expected.ToExpectedObject().ShouldEqual(budgetToLookUp);
-        }
+		[TestMethod]
+		public void The_date_never_be_set()
+		{
+			_startDate = new DateTime(2018, 9, 1);
+			_endDate = new DateTime(2018, 9, 2);
+			TheBudgetShouldBe(0);
+		}
 
-        private void TheBudgetShouldBe(int expected)
-        {
-            Assert.AreEqual(expected, _budgetService.TotalAmount(_startDate, _endDate));
-        }
-    }
+		private void TheBudgetShouldBe(int expected)
+		{
+			Assert.AreEqual(expected, _budgetService.TotalAmount(_startDate, _endDate));
+		}
+	}
 
-    internal class FakeRepository : IBudgetRepository
-    {
-        public List<Budget> GetAll()
-        {
-            return new List<Budget>()
-            {
-                new Budget
-                {
-                    YearMonth = "201801",
-                    Amount = 31
-                },
-                new Budget
-                {
-                    YearMonth = "201804",
-                    Amount = 300
-                },
-                new Budget
-                {
-                    YearMonth = "201805",
-                    Amount = 31
-                },
-                new Budget
-                {
-                    YearMonth = "201901",
-                    Amount = 31
-                }
-            };
-        }
-    }
+	internal class FakeRepository : IBudgetRepository
+	{
+		public List<Budget> GetAll()
+		{
+			return new List<Budget>()
+			{
+				new Budget
+				{
+					YearMonth = "201801",
+					Amount = 31
+				},
+				new Budget
+				{
+					YearMonth = "201804",
+					Amount = 300
+				},
+				new Budget
+				{
+					YearMonth = "201805",
+					Amount = 31
+				},
+				new Budget
+				{
+					YearMonth = "201901",
+					Amount = 31
+				}
+			};
+		}
+	}
 }
