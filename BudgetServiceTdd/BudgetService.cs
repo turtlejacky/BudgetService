328ec4f508b23767f050ed3cd0cd59502a561c39
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-12-01 19:59:41 +0800

    Add code smell comment, extract variable to remove duplication

--- BudgetServiceTdd/BudgetService.cs
+++ BudgetServiceTdd/BudgetService.cs
@@ -4,74 +4,78 @@ using System.Linq;
 
 namespace BudgetServiceTdd
 {
-    public class BudgetService
-    {
-        private readonly Dictionary<string, int> _budgetLookUp;
-        private readonly IBudgetRepository _budgetRepository;
+	public class BudgetService
+	{
+		private readonly Dictionary<string, int> _budgetLookUp;
+		private readonly IBudgetRepository _budgetRepository;
 
-        public BudgetService(IBudgetRepository budgetRepository)
-        {
-            _budgetRepository = budgetRepository;
-            _budgetLookUp = SetBudgetToLookUp();
-        }
+		public BudgetService(IBudgetRepository budgetRepository)
+		{
+			_budgetRepository = budgetRepository;
+			_budgetLookUp = SetBudgetToLookUp();
+		}
 
-        public Dictionary<string, int> SetBudgetToLookUp()
-        {
-            var budgets = _budgetRepository.GetAll();
-            return budgets.ToDictionary(x => x.YearMonth, x => x.Amount / GetDays(x.YearMonth));
-        }
+		public Dictionary<string, int> SetBudgetToLookUp()
+		{
+			var budgets = _budgetRepository.GetAll();
+			return budgets.ToDictionary(x => x.YearMonth, x => x.Amount / GetDays(x.YearMonth));
+		}
 
-        public double TotalAmount(DateTime start, DateTime end)
-        {
-            return start > end ? 0 : GetYearMonthList(start, end).Sum();
-        }
+		public double TotalAmount(DateTime start, DateTime end)
+		{
+			return start > end ? 0 : GetYearMonthList(start, end).Sum();
+		}
 
-        private int GetDays(string yearMonth)
-        {
-            var year = Convert.ToInt32(yearMonth.Substring(0, 4));
-            var month = Convert.ToInt32(yearMonth.Substring(4, 2));
-            return DateTime.DaysInMonth(year, month);
-        }
+		private int GetDays(string yearMonth)
+		{
+			var year = Convert.ToInt32(yearMonth.Substring(0, 4));
+			var month = Convert.ToInt32(yearMonth.Substring(4, 2));
+			return DateTime.DaysInMonth(year, month);
+		}
 
-        private IEnumerable<int> GetYearMonthList(DateTime startDate, DateTime endDate)
-        {
-            for (var year = startDate.Year; year <= endDate.Year; year++)
-            {
-                var startMonth = year == startDate.Year ? startDate.Month : 1;
-                var endMonth = year == endDate.Year ? endDate.Month : 12;
-                for (var month = startMonth; month <= endMonth; month++)
-                {
-                    var yearMonth = year + TransToDateFormat(month);
-                    var budget = _budgetLookUp.ContainsKey(yearMonth) ? _budgetLookUp[yearMonth] : 0;
-                    if (IsLastMonth(endDate, month, year))
-                    {
-                        yield return endDate.Day * budget;
-                    }
-                    else if (IsFirstMonth(startDate, month, year))
-                    {
-                        yield return (DateTime.DaysInMonth(year, month) - startDate.Day + 1) * budget;
-                    }
-                    else
-                    {
-                        yield return DateTime.DaysInMonth(year, month) * budget;
-                    }
-                }
-            }
-        }
+		// primitive obsession, data clump, duplication
+		private IEnumerable<int> GetYearMonthList(DateTime startDate, DateTime endDate)
+		{
+			for (var year = startDate.Year; year <= endDate.Year; year++)
+			{
+				var startMonth = year == startDate.Year ? startDate.Month : 1;
+				var endMonth = year == endDate.Year ? endDate.Month : 12;
+				for (var month = startMonth; month <= endMonth; month++)
+				{
+					var yearMonth = year + TransToDateFormat(month);
+					var dailyAmount = _budgetLookUp.ContainsKey(yearMonth) ? _budgetLookUp[yearMonth] : 0;
+					if (IsLastMonth(endDate, month, year))
+					{
+						var intervalDays = endDate.Day;
+						yield return intervalDays * dailyAmount;
+					}
+					else if (IsFirstMonth(startDate, month, year))
+					{
+						var intervalDays = (DateTime.DaysInMonth(year, month) - startDate.Day + 1);
+						yield return intervalDays * dailyAmount;
+					}
+					else
+					{
+						var intervalDays = DateTime.DaysInMonth(year, month);
+						yield return intervalDays * dailyAmount;
+					}
+				}
+			}
+		}
 
-        private bool IsFirstMonth(DateTime start, int month, int year)
-        {
-            return month == start.Month && year == start.Year;
-        }
+		private bool IsFirstMonth(DateTime start, int month, int year)
+		{
+			return month == start.Month && year == start.Year;
+		}
 
-        private bool IsLastMonth(DateTime endDate, int month, int year)
-        {
-            return month == endDate.Month && year == endDate.Year;
-        }
+		private bool IsLastMonth(DateTime endDate, int month, int year)
+		{
+			return month == endDate.Month && year == endDate.Year;
+		}
 
-        private string TransToDateFormat(int i)
-        {
-            return i < 10 ? "0" + i : i.ToString();
-        }
-    }
+		private string TransToDateFormat(int i)
+		{
+			return i < 10 ? "0" + i : i.ToString();
+		}
+	}
 }
